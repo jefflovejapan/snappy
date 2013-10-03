db836710f7a56705000a7220639432b6efb4022f
Jeff Blagdon <jeffblagdon@gmail.com>
2013-10-03 11:22:02 -0400

    Deleted all the UDP stuff.

--- snappy.py
+++ snappy.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 
-import socket, sys, requests, cmd, readline, json
+import sys, requests, cmd, json, os
 from bs4 import BeautifulSoup
 
-#The parameters are important -- UDP, not TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-MAX = 65535
-PORT = 1900
 
 class snappyPrompt(cmd.Cmd):
 
@@ -17,41 +12,59 @@ class snappyPrompt(cmd.Cmd):
 	ns_error = '*** Not supported by this camera'
 	camera_str = 'camera'
 
-	intro = 'Welcome to Snappy. Proceed when you\'re connected to your camera.'
+	intro = 'Welcome to Snappy. Let\'s take some pictures!'
+
+
 	def do_snap(self, input_string):
 		if (self.camera_str not in self.services):
 			print self.ns_error
 		else:
 			target_url = ''.join([self.url, '/', self.camera_str])
-			print target_url
 			command_dict = {'method': 'actTakePicture', 'params': [], 'id': 1, 'version': '1.0'}
 			command_json = json.dumps(command_dict)
 			r = requests.post(target_url, command_json)
-			print r.content
+			
+			#loads the json string into a python dictionary
+			response_dict = json.loads(r.text)
+			image_url = str(response_dict.get('result')[0][0])
 
-	def do_viewstream(self, input_string):
-		print 'Starting the livestream'
-	def do_printurl(self, input_string):
-		#when referring to attributes, need to preface with self (OOP)
-		print self.url
-	def do_printservices(self, input_string):
-		#same here
-		for i in self.services:
-			print i
+			open_result(image_url)
 
+
+
+	def do_timer(self, input_string):
+		if (self.camera_str not in self.services):
+			print self.ns_error
+		else:
+			target_url = ''.join([self.url, '/', self.camera_str])
+			print target_url
+			command_dict = {'method': 'getSupportedSelfTimer', 'params': [], 'id': 1, 'version': '1.0'}
+			command_json = json.dumps(command_dict)
+			r = requests.post(target_url, command_json)
+			
+			#loads the json string into a python dictionary
+			response_dict = json.loads(r.text)
+			timer_choices = str(response_dict.get('result')[0])
+			
+			print timer_choices
+
+			while True:
+				prompt_str = 'How many seconds'
+				s = raw_input(' '.join(['How many seconds? ', str(timer_choices)]))
+				if s in timer_choices:
+					break
+
+			command_dict = {'method': 'setSelfTimer', 'params': [s], 'id': 1, 'version': '1.0'}
+			command_json = json.dumps(command_dict)
+			r = requests.post(target_url, command_dict)
+			print r.text
+
+
+
+def open_result(url_string):
+	os.system(' '.join(['open', url_string]))
 
 def get_description():
-	message = ('M-SEARCH * HTTP/1.1\r\n' +
-	'HOST: 239.255.255.250:1900\r\n' +
-	'MAN: "ssdp:discover"\r\n' +
-	'MX: 10\r\n' +
-	'ST: urn:schemas-sony-com:service:ScalarWebAPI:1\r\n' +
-	'USER-AGENT: snappy.py\r\n\r\n')
-
-	s.sendto(message, ('239.255.255.250', PORT))
-	print 'Address after sending', s.getsockname()
-	data, address = s.recvfrom(MAX) # overly promiscuous!
-	print 'The server', address, 'says', repr(data)
 	return requests.get('http://10.0.0.1:64321/DmsRmtDesc.xml')
 
 
@@ -61,7 +74,6 @@ def GetURL(xml):
 	#The search string needs to be lower case
 	alu = xml.find('av:x_scalarwebapi_actionlist_url')
 	if alu:
-		print alu
 		return alu.text
 	else:
 		raise Exception('The camera isn\'t supplying a URL')
